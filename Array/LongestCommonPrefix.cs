720d023708357656684516230a1eb17f6f0bb5f4
mostafa3537 <mostafa.elsayed3537@gmail.com>
2025-11-14 22:04:54 +0200

    LongestCommonPrefix

---
+++ Array/LongestCommonPrefix.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array;
+
+internal class LongestCommonPrefixProblem
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var result = "";
+
+        var firstWord = strs[0];
+
+        var firstWordcharchters = firstWord.Select(x => x).ToList();
+
+        var smallestWordLength = strs.MinBy(x => x.Length).Count();
+
+        firstWordcharchters.RemoveRange(smallestWordLength, firstWordcharchters.Count - smallestWordLength);
+
+        for (int charcter = 0; charcter < firstWordcharchters.Count; charcter++)
+        {
+            var isAdd = true;
+
+            for (int word = 0; word < strs.Length; word++)
+            {
+                if (word == 0)
+                {
+                    continue;
+                }
+                if (strs[word].Length < firstWordcharchters.Count)
+                {
+                    return result;
+                }
+                // check if all words have the same charcter at the current position
+                if (strs[word][charcter] != firstWordcharchters[charcter])
+                {
+                    isAdd = false;
+                    return result;
+                }
+            }
+
+            if (isAdd)
+            {
+                result += firstWordcharchters[charcter];
+            }
+        }
+
+        return result;
+    }
+    public static string LongestCommonPrefixOptimized(string[] strs)
+    {
+        var result = "";
+
+        var firstWord = strs[0];
+
+        for (int i = 0; i < firstWord.Length; i++)
+        {
+            char currentChar = firstWord[i];
+
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i > strs[j].Length || strs[j][i] != currentChar)
+                {
+                    result = firstWord.Substring(0, i);
+                    return result;
+                }
+
+            }
+
+        }
+
+        return result;
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -4,94 +4,10 @@ namespace Array;
 
 internal class Program
 {
-    public static int MaxArea(int[] height)
-    {
-
-        if (2 > height.Length || height.Length > Math.Pow(10, 5))
-        {
-            return 0;
-        }
-        var maxArea = 0;
-
-        for (int i = 0; i < height.Length; i++)
-        {
-            if (height[i] < 0 || height[i] > Math.Pow(10, 4))
-            {
-                return 0;
-            }
-            for (int j = 0; j < height.Length; j++)
-            {
-                int currentArea;
-                if (i == j)
-                {
-                    continue;
-                }
-                if (height[i] < height[j])
-                {
-                    currentArea = height[i] * Math.Abs(i - j);
-                }
-                else
-                {
-                    currentArea = height[j] * Math.Abs(i - j);
-                }
-
-                if (currentArea > maxArea)
-                {
-                    maxArea = currentArea;
-                }
-            }
-        }
-        return maxArea;
-    }
 
 
-
-    public static int MaxAreaOptimized(int[] height)
-    {
-
-        if (2 > height.Length || height.Length > Math.Pow(10, 5))
-        {
-            return 0;
-        }
-        var maxArea = 0;
-
-        var left = 0;
-
-        var right = height.Length - 1;
-
-        while (left < right) 
-        {
-            // will move the pointer which has the smaller height
-
-            int currentArea;
-            if (height[left] < height[right])
-            {
-                currentArea = height[left] * (right - left);
-                if (currentArea > maxArea)
-                {
-                    maxArea = currentArea;
-                }
-                left++;
-            }
-            else
-            {
-                currentArea = height[right] * (right - left);
-                if (currentArea > maxArea)
-                {
-                    maxArea = currentArea;
-                }
-                right --;
-            }
-        }
-
-        return maxArea;
-    }
     static void Main(string[] args)
     {
-        var input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
-
-
-        var result = MaxAreaOptimized(input);
         Console.WriteLine(result);
     }
 }
