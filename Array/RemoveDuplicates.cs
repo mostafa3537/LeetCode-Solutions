270d054a8bbad22ce594c904f3c406ce77c72333
Mostafa Ahmed Elsayed <melsayed@RASAN.CO>
2025-11-16 17:32:09 +0200

    RemoveDuplicates

--- Array/RemoveDuplicates.cs
+++ Array/RemoveDuplicates.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿namespace Array;
 
-namespace Array;
-
-internal class Program
+internal class RemoveDuplicatesProb
 {
 	public static int RemoveDuplicates(int[] nums)
 	{
@@ -29,7 +26,7 @@ internal class Program
 		if (nums.Length == 0)
 			return 0;
 
-		int k = 1;  
+		int k = 1;
 
 		for (int i = 1; i < nums.Length; i++)
 		{
@@ -42,14 +39,4 @@ internal class Program
 
 		return k;
 	}
-
-	static void Main(string[] args)
-	{
-		var input = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-
-		var result = RemoveDuplicatesOptimize(input);
-
-		Console.WriteLine(result);
-	}
 }
-
