aa2c1906c1fc1dd2b47c4ff2d81c188c257c25c0
Mostafa Ahmed Elsayed <melsayed@RASAN.CO>
2025-11-24 15:02:13 +0200

    ThreeSum

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,34 +1,17 @@
-﻿using System;
-using System.Linq;
-
-namespace Array;
+﻿namespace Array;
 
 internal class Program
 {
-	public static int RemoveElement(int[] nums, int val)
-	{
-		int k = 0;
-
-		for (int i = 0; i < nums.Length; i++)
-		{
-			if (nums[i] != val)
-			{
-				nums[k] = nums[i];
-				k++;
-			}
-		}
-
-		return k;
-	}
+	
 
 	static void Main(string[] args)
 	{
-		var nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
-		var val = 2;
+		//var nums = new int[] { -1, 0, 1, 2, -1, -4 };
+		////var nums = new int[] { -4, -1, -1, 0, 1, 2 };
 
-		var result = RemoveElement(nums, val);
+		//var result = ThreeSum(nums);
 
-		Console.WriteLine(result);
+		//Console.WriteLine(result);
 	}
 }
 
---
+++ Array/ThreeSum.cs
@@ -0,0 +1,48 @@
+﻿namespace Array;
+
+internal class ThreeSum
+{
+	public static IList<IList<int>> ThreeSumProb(int[] nums)
+	{
+		var result = new List<IList<int>>();
+		var sorted = nums.OrderBy(x => x).ToArray();
+
+		for (int i = 0; i < sorted.Length - 2; i++)
+		{
+			if (i > 0 && sorted[i] == sorted[i - 1])
+				continue;
+
+			var left = i + 1;
+			var right = sorted.Length - 1;
+
+			while (left < right)
+			{
+				var sum = sorted[i] + sorted[left] + sorted[right];
+
+				if (sum == 0)
+				{
+					result.Add(new List<int>() { sorted[i], sorted[left], sorted[right] });
+
+					while (left < right && sorted[left] == sorted[left + 1])
+						left++;
+
+					while (left < right && sorted[right] == sorted[right - 1])
+						right--;
+
+					left++;
+					right--;
+				}
+				else if (sum > 0)
+				{
+					right--;
+				}
+				else
+				{
+					left++;
+				}
+			}
+		}
+
+		return result;
+	}
+}
