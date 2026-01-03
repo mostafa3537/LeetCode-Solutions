87b9c098ca37dae137ece59737742ff073cc19a3
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-01-03 02:29:04 +0200

    TwoSum

--- Array/Program.cs
+++ Array/Program.cs
@@ -3,46 +3,18 @@
 internal class Program
 {
 
-    public static IList<IList<int>> CombinationSum(int[] candidates, int target)
+    public static int[] TwoSum(int[] nums, int target)
     {
-        Array.Sort(candidates);
-        var result = new List<IList<int>>();
+        var res = new int[] { };
+ 
 
-        var stack = new Stack<(List<int> combo, int sum, int index)>();
-        stack.Push((new List<int>(), 0, 0));
-
-        while (stack.Count > 0)
-        {
-            var (combo, sum, index) = stack.Pop();
-
-            if (sum == target)
-            {
-                result.Add(combo);
-                continue;
-            }
-
-            for (int i = index; i < candidates.Length; i++)
-            {
-                int num = candidates[i];
-                int newSum = sum + num;
-
-                if (newSum > target)
-                    break;
-
-                var newCombo = new List<int>(combo) { num };
-                stack.Push((newCombo, newSum, i));
-            }
-        }
-
-        return result;
+        return res;
     }
-
-
     static void Main(string[] args)
     {
-        var nums = new int[] { 2, 3, 6, 7 };
-        var target = 7;
-        var result = CombinationSum(nums, target);
+        var nums = new int[] { 2, 7, 11, 15 };
+        var target = 9;
+        var result = TwoSum(nums, target);
 
         Console.WriteLine(result);
     }
---
+++ Array/TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array;
+
+internal class TwoSumProblem
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var res = new int[] { };
+
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+
+            if (dict.ContainsKey(complement))
+            {
+                res = new int[] { dict[complement], i };
+                break;
+            }
+            dict[nums[i]] = i;
+
+        }
+
+        return res;
+    }
+}
