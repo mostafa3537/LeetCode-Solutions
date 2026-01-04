dacb34e225f0379e2d94cac98067085108ecc648
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-01-04 07:58:17 +0200

    SubarraySum

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,35 +1,44 @@
-﻿namespace Arrayprob;
+﻿using System.Linq;
+
+namespace Arrayprob;
 
 internal class Program
 {
-
-    public static int MaximumDifference(int[] nums)
+    public static int SubarraySum(int[] nums, int k)
     {
-        var minimumSoFar = nums[0];
-        var maxDifference = -1;
+        var result = 0;
+        var prefixSumDictionary = new Dictionary<int, int>();
+        var prefixSum = 0;
 
-        for (int i = 1; i < nums.Length; i++)
+        prefixSumDictionary.Add(0, 1); // To handle the case when prefixSum itself is equal to k
+        for (int i = 0; i < nums.Length; i++)
         {
-            var currentNumber = nums[i];
+            prefixSum += nums[i];
 
-            if (currentNumber <= minimumSoFar)
+            if (prefixSumDictionary.TryGetValue(prefixSum - k, out int count))
             {
-                minimumSoFar = currentNumber;
+                result = result + count;
+            }
+
+            if (prefixSumDictionary.ContainsKey(prefixSum))
+            {
+                prefixSumDictionary[prefixSum]++;
             }
             else
             {
-                maxDifference = Math.Max(maxDifference, currentNumber - minimumSoFar);
+                prefixSumDictionary.Add(prefixSum, 1);
             }
-
         }
 
-        return maxDifference;
+        return result;
     }
     static void Main(string[] args)
     {
-        var nums = new int[] { 999, 997, 980, 976, 948, 940, 938, 928, 924, 917, 907, 907, 881, 878, 864, 862, 859, 857, 848, 840, 824, 824, 824, 805, 802, 798, 788, 777, 775, 766, 755, 748, 735, 732, 727, 705, 700, 697, 693, 679, 676, 644, 634, 624, 599, 596, 588, 583, 562, 558, 553, 539, 537, 536, 509, 491, 485, 483, 454, 449, 438, 425, 403, 368, 345, 327, 287, 285, 270, 263, 255, 248, 235, 234, 224, 221, 201, 189, 187, 183, 179, 168, 155, 153, 150, 144, 107, 102, 102, 87, 80, 57, 55, 49, 48, 45, 26, 26, 23, 15 };
+        //var nums = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        var nums = new int[] { 9, -9, 2, 3, 4 };
+        var target = 9;
 
-        var result = MaximumDifference(nums);
+        var result = SubarraySum(nums, target);
 
         Console.WriteLine(result);
     }
---
+++ Array/SubarraySum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class SubarraySumProblem
+    {
+        public static int SubarraySum(int[] nums, int k)
+        {
+            var result = 0;
+            var prefixSumDictionary = new Dictionary<int, int>();
+            var prefixSum = 0;
+
+            prefixSumDictionary.Add(0, 1); // To handle the case when prefixSum itself is equal to k
+            for (int i = 0; i < nums.Length; i++)
+            {
+                prefixSum += nums[i];
+
+                if (prefixSumDictionary.TryGetValue(prefixSum - k, out int count))
+                {
+                    result = result + count;
+                }
+
+                if (prefixSumDictionary.ContainsKey(prefixSum))
+                {
+                    prefixSumDictionary[prefixSum]++;
+                }
+                else
+                {
+                    prefixSumDictionary.Add(prefixSum, 1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
