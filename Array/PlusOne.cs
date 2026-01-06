6300b7d95a8cf35ef282bc04333d814351063503
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-01-06 04:54:19 +0200

    MaxSlidingWindow

--- Array/PlusOne.cs
+++ Array/PlusOne.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Arrayprob
+namespace ArrayProblems
 {
     internal class PlusOne
     {
--- Array/Program.cs
+++ Array/Program.cs
@@ -1,44 +1,43 @@
 ﻿using System.Linq;
 
-namespace Arrayprob;
+namespace ArrayProblems;
 
 internal class Program
 {
-    public static int SubarraySum(int[] nums, int k)
+    public static int[] MaxSlidingWindow(int[] nums, int k)
     {
-        var result = 0;
-        var prefixSumDictionary = new Dictionary<int, int>();
-        var prefixSum = 0;
+        if (nums.Length == 0 || k == 0)
+            return Array.Empty<int>();
 
-        prefixSumDictionary.Add(0, 1); // To handle the case when prefixSum itself is equal to k
+        var result = new int[nums.Length - k + 1];
+        var deque = new LinkedList<int>(); // stores indices
+
         for (int i = 0; i < nums.Length; i++)
         {
-            prefixSum += nums[i];
+            // 1. Remove indices outside the window
+            if (deque.Count > 0 && deque.First.Value <= i - k)
+                deque.RemoveFirst();
 
-            if (prefixSumDictionary.TryGetValue(prefixSum - k, out int count))
-            {
-                result = result + count;
-            }
+            // 2. Remove smaller values from the back
+            while (deque.Count > 0 && nums[deque.Last.Value] < nums[i])
+                deque.RemoveLast();
 
-            if (prefixSumDictionary.ContainsKey(prefixSum))
-            {
-                prefixSumDictionary[prefixSum]++;
-            }
-            else
-            {
-                prefixSumDictionary.Add(prefixSum, 1);
-            }
+            // 3. Add current index
+            deque.AddLast(i);
+
+            // 4. Record max once window is ready
+            if (i >= k - 1)
+                result[i - k + 1] = nums[deque.First.Value];
         }
 
         return result;
     }
     static void Main(string[] args)
     {
-        //var nums = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-        var nums = new int[] { 9, -9, 2, 3, 4 };
-        var target = 9;
+        var nums = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
+        var target = 3;
 
-        var result = SubarraySum(nums, target);
+        var result = MaxSlidingWindow(nums, target);
 
         Console.WriteLine(result);
     }
