a2c66e91307e574c7562ac0c612acb75bf08bf9d
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-01-08 08:41:29 +0200

    MaxSubarraySumCircular

---
+++ Array/MaxSubarraySumCircularProblem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems
+{
+    internal class MaxSubarraySumCircularProblem
+    {
+        public static int MaxSubarraySumCircular(int[] nums)
+        {
+            // result = max bet (maxSum , sum - minSum)
+
+            var result = 0;
+
+            var max = MaxSum(nums);
+            var sum = Sum(nums);
+            var min = MinSum(nums);
+
+            result = Math.Max(MaxSum(nums), Sum(nums) - MinSum(nums));
+
+
+
+            return result;
+        }
+
+        private static int MaxSum(int[] nums)
+        {
+            var result = nums[0];
+            var sum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sum = Math.Max(sum + nums[i], nums[i]);
+
+                result = Math.Max(result, sum);
+            }
+
+            return result;
+        }
+        private static int Sum(int[] nums)
+        {
+            var sum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sum = sum + nums[i];
+            }
+
+            return sum;
+        }
+        private static int MinSum(int[] nums)
+        {
+            var result = nums[0];
+            var sum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                sum = Math.Min(sum + nums[i], nums[i]);
+
+                result = Math.Min(result, sum);
+            }
+
+            return result;
+        }
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -5,7 +5,24 @@ namespace ArrayProblems;
 
 internal class Program
 {
-    public static int MaxSubArray(int[] nums)
+    public static int MaxSubarraySumCircular(int[] nums)
+    {
+        // result = max bet (maxSum , sum - minSum)
+
+        var result = 0;
+
+        var max = MaxSum(nums);
+        var sum = Sum(nums);
+        var min = MinSum(nums);
+
+        result = Math.Max(MaxSum(nums), Sum(nums) - MinSum(nums));
+
+ 
+
+        return result;
+    }
+
+    private static int MaxSum(int[] nums)
     {
         var result = nums[0];
         var sum = nums[0];
@@ -19,12 +36,37 @@ internal class Program
 
         return result;
     }
+    private static int Sum(int[] nums)
+    {
+        var sum = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            sum = sum + nums[i];
+        }
+
+        return sum;
+    }
+    private static int MinSum(int[] nums)
+    {
+        var result = nums[0];
+        var sum = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            sum = Math.Min(sum + nums[i], nums[i]);
+
+            result = Math.Min(result, sum);
+        }
+
+        return result;
+    }
 
     static void Main(string[] args)
     {
-        var nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        var nums = new int[] { -3, -2, -3 };
         ;
-        var result = MaxSubArray(nums);
+        var result = MaxSubarraySumCircular(nums);
 
         Console.WriteLine(result);
     }
