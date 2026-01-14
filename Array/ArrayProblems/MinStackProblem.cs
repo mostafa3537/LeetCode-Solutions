da23ebac5cbc64854ff7d941b4d6e3ec2cd8fccb
Mostafa Ahmed Elsayed <melsayed@RASAN.CO>
2026-01-14 18:47:08 +0200

    MinStack

---
+++ Array/ArrayProblems/MinStackProblem.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.ArrayProblems
+{
+	internal class MinStackProblem
+	{
+		public class MinStack
+		{
+			private List<int> _stack { get; set; } = new List<int>();
+			private int _min { get; set; } = 0;
+			public MinStack()
+			{
+				_stack = new List<int> { };
+			}
+
+			public void Push(int val)
+			{
+				if (_stack.Count == 0)
+				{
+					_min = val;
+				}
+				else
+				{
+					if (val < _min)
+					{
+						_min = val;
+					}
+				}
+				_stack.Add(val);
+
+			}
+
+			public void Pop()
+			{
+				if (_stack.Count == 0)
+				{
+					_min = 0;
+					return;
+				}
+				var lastIndex = _stack.Count - 1;
+				_stack.RemoveAt(lastIndex);
+
+				if (_stack.Count > 0)
+				{
+					_min = _stack.Min();
+
+				}
+
+			}
+
+			public int Top()
+			{
+				return _stack[^1];
+			}
+
+			public int GetMin()
+			{
+				return _min;
+			}
+		}
+	}
+
+	public class MinStackEnhanced
+	{
+		private Stack<int> stack = new();
+		private Stack<int> minStack = new();
+
+		public void Push(int val)
+		{
+			stack.Push(val);
+
+			if (minStack.Count == 0 || val <= minStack.Peek())
+				minStack.Push(val);
+		}
+
+		public void Pop()
+		{
+			if (stack.Pop() == minStack.Peek())
+				minStack.Pop();
+		}
+
+		public int Top()
+		{
+			return stack.Peek();
+		}
+
+		public int GetMin()
+		{
+			return minStack.Peek();
+		}
+	}
+
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -1,51 +1,96 @@
-﻿namespace Problems;
+﻿using System.Linq;
+
+namespace Problems;
 
 internal class Program
 {
-	public static int IsAnagram(int[] nums)
+	public static void IsAnagram(int[] nums)
 	{
 
-		// sort the []
-		//loop over if the next element is missing or return the next number of the []
-
-		if (nums.Length == 1)
-		{
-			if (nums[0] <= 0)
-			{
-				return nums[^1] + 1;
-			}
-			else {
-				return nums[^1] - 1;
-			}
-		}
-
-		Array.Sort(nums);
-
-		for (int i = 0; i < nums.Length; i++)
-		{
-			if (nums[0] > 0)
-			{
-				return nums[0] - 1;
-			}
-			if (i < nums.Length - 1)
-			{
-				if (nums[i + 1] != nums[i] + 1)
-				{
-					return nums[i] + 1;
-				}
-			}
-
-		}
-
-
-		return nums[^1] + 1;
+ 
 	}
 	static void Main(string[] args)
 	{
-		var s = new int[] { 1, 2 };
+		//[ "getMin","pop","top","getMin","pop","getMin","pop"]
+		MinStack minStack = new MinStack();
+		minStack.Push(-2);
+		minStack.Push(0);
+		minStack.Push(-3);
+		minStack.GetMin(); // return -3
+		minStack.Pop();
+		minStack.GetMin(); // return -3
+		minStack.Top();    // return 0
+		minStack.GetMin(); // return -2
+		minStack.Pop();
+		minStack.Push(0);
+		minStack.Push(0);
+		minStack.Push(0);
+		minStack.GetMin(); // return -3
+		minStack.Pop();
+		minStack.Top();    // return 0
+		minStack.GetMin(); // return -3
+		minStack.Pop();
+		minStack.GetMin(); // return -3
+		minStack.Pop();
 
-		var result = IsAnagram(s);
+		//var result = IsAnagram(s);
 
-		Console.WriteLine(result);
+		//Console.WriteLine(result);
 	}
+
+	public class MinStack
+	{
+		private List<int> _stack { get; set; }= new List<int>();
+		private int _min{ get; set; } = 0;
+		public MinStack()
+		{
+			_stack = new List<int> { };
+		}
+
+		public void Push(int val)
+		{
+			if (_stack.Count == 0)
+			{
+				_min = val;
+			}
+			else
+			{
+				if (val < _min)
+				{
+					_min = val;
+				}
+			}
+			_stack.Add(val);
+
+		}
+
+		public void Pop()
+		{
+			if (_stack.Count == 0) 
+			{ 
+				_min = 0;
+				return;
+			}
+			var lastIndex = _stack.Count - 1;
+			_stack.RemoveAt(lastIndex);
+
+			if (_stack.Count > 0)
+			{
+				_min = _stack.Min();
+
+			}
+
+		}
+
+		public int Top()
+		{
+			return _stack[^1];
+		}
+
+		public int GetMin()
+		{
+			return _min;
+		}
+	}
+
 }
