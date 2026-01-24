647edecb2aacf692cf642e2690d05c78ca7d239f
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-01-24 20:58:02 +0200

    NumMatchingSubseq

--- Array/Program.cs
+++ Array/Program.cs
@@ -4,62 +4,59 @@ namespace Problems;
 
 internal class Program
 {
-    public static bool IsAlienSorted(string[] words, string order)
+    public static int NumMatchingSubseq(string s, string[] words)
     {
-        // make a dic with all the char and it' priority
-        // loop over the words compare each one on with the next thier first letter 
-        // if its the same for all letters take the shorter
+        var result = 0;
 
-        var dic = new Dictionary<char, int>();
-
-        for (int i = 0; i < order.Length; i++)
+        for (int i = 0; i < words.Length; i++)
         {
-            dic.Add(order[i], i);
-        }
-
-        for (int i = 0; i < words.Length - 1; i++)
-        {
-            var word = words[i];
-            var nextWord = words[i + 1];
-
-            for (int j = 0; j < word.Length; j++)
+            if (IsSubsequence(words[i], s))
             {
-                if (dic.TryGetValue(word[j], out int val) && dic.TryGetValue(nextWord[j], out int nextVal))
-                {
-                    if (nextVal < val)
-                    {
-                        return false;
-                    }
-                    else if (nextVal == val)
-                    {
-                        if (j == word.Length-1 || j == nextWord.Length - 1)
-                        {
-                            if (word.Length > nextWord.Length)
-                            {
-                                return false;
-                            }
-                        }
-                        continue;   
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
+                result++;
             }
-
         }
-
-        return true;
+        return result;
     }
 
+    private static bool IsSubsequence(string s, string t)
+    {
+        if (s.Length > t.Length)
+        {
+            return false;
+        }
+
+        var sPointer = 0;
+        var tPointer = 0;
+
+        while (sPointer < s.Length && tPointer < t.Length)
+        {
+
+            if (s[sPointer] == t[tPointer])
+            {
+                sPointer++;
+                tPointer++;
+            }
+            else
+            {
+                tPointer++;
+
+            }
+        }
+
+        if (sPointer != s.Length)
+        {
+            return false;
+
+        }
+        return true;
+
+    }
     static void Main(string[] args)
     {
-        var words = new string[] { "apple", "app" };
-        var order = "abcdefghijklmnopqrstuvwxyz";
+        var words = new string[] { "a", "bb", "acd", "ace" };
+        var s = "abcde";
 
-        var result = IsAlienSorted(words, order);
+        var result = NumMatchingSubseq(s, words);
 
         Console.WriteLine(result);
     }
---
+++ Array/StringProblems/NumMatchingSubseqProblem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.StringProblems
+{
+    internal class NumMatchingSubseqProblem
+    {
+        public static int NumMatchingSubseq(string s, string[] words)
+        {
+            var result = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (IsSubsequence(words[i], s))
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+        private static bool IsSubsequence(string s, string t)
+        {
+            if (s.Length > t.Length)
+            {
+                return false;
+            }
+
+            var sPointer = 0;
+            var tPointer = 0;
+
+            while (sPointer < s.Length && tPointer < t.Length)
+            {
+
+                if (s[sPointer] == t[tPointer])
+                {
+                    sPointer++;
+                    tPointer++;
+                }
+                else
+                {
+                    tPointer++;
+
+                }
+            }
+
+            if (sPointer != s.Length)
+            {
+                return false;
+
+            }
+            return true;
+
+        }
+    }
+}
