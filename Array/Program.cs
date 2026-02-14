8181cf42ff9c966fbc8819c06982385c151cd77d
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-02-14 18:52:46 +0200

    Valid Parentheses

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,75 +1,43 @@
-﻿using System.Linq;
-
-namespace Problems;
+﻿namespace Problems;
 
 internal class Program
 {
-	public static int LargestSubmatrix(int[][] matrix)
-	{
-		// make histogram for each row
-		// reorder it desc
-		// cal Area & track max
+    public static int ExclusiveTime(string s)
+    {
+        var stack = new Stack<char>();
 
-		int maxArea = 0;
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '(':
+                    stack.Push(c);
+                    break;
+                case ')':
+                    if (stack.TryPeek(out char res) && res == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(c);
 
-		var length = matrix.Length;
-		var collength = matrix[0].Length;
+                    }
+                    break;
 
-		var colCount = new int[collength];
+            }
+        }
 
-		for (int row = 0; row < length; row++)
-		{
-			for (int col = 0; col < collength; col++)
-			{
-				if (matrix[row][col] == 1)
-				{
-					colCount[col]++;
+        return stack.Count;
+    }
 
-				}
-				else
-				{
-					colCount[col] = 0;
-				}
-			}
 
-			var reorderd = ReorderHistogram(colCount);
-			var area = CalCulateArea(reorderd);
+    static void Main(string[] args)
+    {
+        var path = "(((";
 
-			maxArea = Math.Max(maxArea, area);
-		}
+        var res = ExclusiveTime(path);
 
-		return maxArea;
-	}
-
-	private static int CalCulateArea(int[] reorderd)
-	{
-		var maxArea = 0;
-
-		for (int i = 0; i < reorderd.Length; i++)
-		{
-			var area = 0;
-
-			area = reorderd[i] * (i + 1);
-
-			maxArea = Math.Max(maxArea, area);
-		}
-
-		return maxArea;
-	}
-
-	private static int[] ReorderHistogram(int[] colCount)
-	{
-		var res = colCount.OrderByDescending(x => x).ToArray();
-
-		return res;
-	}
-
-	static void Main(string[] args)
-	{
-		var path = new int[][] { [1, 0, 1, 0, 1] };
-
-		var res = LargestSubmatrix(path);
-
-		Console.WriteLine(res);
-	}
+        Console.WriteLine(res);
+    }
 }
---
+++ Array/Stack/ValidParenthesesProblem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Stack
+{
+    internal class ValidParenthesesProblem
+    {
+        public bool IsValid(string s)
+        {
+            var result = true;
+
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case '(':
+                        stack.Push(c);
+                        break;
+                    case ')':
+                        if (stack.Count == 0 || stack.Pop() != '(')
+                        {
+                            return false;
+                        }
+                        break;
+
+                    case '[':
+                        stack.Push(c);
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Pop() != '[')
+                        {
+                            return false;
+                        }
+                        break;
+
+                    case '{':
+                        stack.Push(c);
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Pop() != '{')
+                        {
+                            return false;
+                        }
+                        break;
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                return false;
+            }
+
+
+            return result;
+        }
+    }
+}
