1c42924dff94ebc7b0ea78fe477f06b1474b9849
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-02-14 18:53:31 +0200

    MinAddToMakeValid

---
+++ Array/Stack/MinAddToMakeValidProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Stack
+{
+    internal class MinAddToMakeValidProblem
+    {
+        public int MinAddToMakeValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case '(':
+                        stack.Push(c);
+                        break;
+                    case ')':
+                        if (stack.TryPeek(out char res) && res == '(')
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            stack.Push(c);
+
+                        }
+                        break;
+
+                }
+            }
+
+            return stack.Count;
+        }
+    }
+}
