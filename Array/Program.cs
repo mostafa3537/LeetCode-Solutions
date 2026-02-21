9597ece557c2fc579fa7d2830b710febc7d4d806
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-02-21 21:39:39 +0200

    LengthOfLastWord

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,60 +1,26 @@
 ﻿namespace Problems;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 internal class Program
 {
-    public static int MaxPerformance(int n, int[] speed, int[] efficiency, int k)
+    public static int LengthOfLastWord(string s)
     {
-        long result = 0;
+        var words = new List<string>();
 
-        // Create a list of tuples to store speed and efficiency together
+        s.Split(' ')
+            .Where(x => !string.IsNullOrEmpty(x))
+            .ToList().ForEach(x => words.Add(x));
 
-        var engineers = new List<(int Speed, int Efficiency)>();
-
-        for (int i = 0; i < n; i++)
-        {
-            engineers.Add((speed[i], efficiency[i]));
-        }
-
-        // Sort engineers by efficiency in descending order
-
-        engineers.Sort((a, b) => b.Efficiency.CompareTo(a.Efficiency));
-
-        // Use a min-heap to keep track of the k highest speeds
-
-        var minHeap = new PriorityQueue<int, int>();
-
-        long speedSum = 0;
-
-        foreach (var engineer in engineers)
-        {
-            // Add the current engineer's speed to the heap and update the speed sum
-            minHeap.Enqueue(engineer.Speed, engineer.Speed);
-            speedSum += engineer.Speed;
-            // If we have more than k engineers, remove the one with the lowest speed
-            if (minHeap.Count > k)
-            {
-                int lowestSpeed = minHeap.Dequeue();
- 
-                speedSum -= lowestSpeed;
-            }
-            // Calculate the performance with the current efficiency and speed sum
-            long performance = speedSum * engineer.Efficiency;
-            result = Math.Max(result, performance);
-        }
-
-        return (int)(result % 1_000_000_007);
+        return words[^1].Length;
     }
 
 
     static void Main(string[] args)
     {
-        var n = 5;
-        var speed = new int[] { 10, 10, 7, 9, 8 };
-        var efficiency = new int[] { 9, 8, 3, 6, 9 };
-        var k = 1;
+        var speed = "   fly me   to   the moon  ";
 
-        var res = MaxPerformance(n, speed, efficiency, k);
+        var res = LengthOfLastWord(speed);
 
         Console.WriteLine(res);
     }
---
+++ Array/StringProblems/LengthOfLastWordProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.StringProblems
+{
+    internal class LengthOfLastWordProblem
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            var words = new List<string>();
+
+            s.Split(' ')
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToList().ForEach(x => words.Add(x));
+
+            return words[^1].Length;
+        }
+    }
+}
