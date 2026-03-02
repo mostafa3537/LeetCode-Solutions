2e82a6dfef1005e55b6dbd95b599c98b0b30083b
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-03-02 06:13:05 +0200

    InorderTraversal

---
+++ Array/BinaryTreeProblems/InorderTraversalProblem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.BinaryTreeProblems
+{
+    internal class InorderTraversalProblem
+    {
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            var res = new List<int>();
+
+            DFS(root, res);
+
+            return res;
+        }
+
+        private static void DFS(TreeNode node, List<int>? res)
+        {
+            if (node == null) return;
+            DFS(node.left, res);
+            res.Add(node.val);
+            DFS(node.right, res);
+        }
+
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -7,84 +7,46 @@ using System.Xml.Linq;
 
 internal class Program
 {
-    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    public static IList<int> InorderTraversal(TreeNode root)
     {
+        var res = new List<int>();
 
-        if (list1 == null)
-        {
-            return list2;
-        }
-        if (list2 == null)
-        {
-            return list1;
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
+        DFS(root, res);
 
-        var current1 = list1;
-        var current2 = list2;
-
-        while (current1 != null && current2 != null)
-        {
-            if (current1.val <= current2.val)
-            {
-                head.next = new ListNode(current1.val);
-                current1 = current1.next;
-
-
-            }
-            else
-            {
-                head.next = new ListNode(current2.val);
-                current2 = current2.next;
-
-            }
-            head = head.next;
-        }
-
-        while (current1 != null)
-        {
-
-            head.next = new ListNode(current1.val);
-            head = head.next;
-            current1 = current1.next;
-
-        }
-
-        while (current1 != null)
-        {
-            head.next = new ListNode(current2.val);
-            head = head.next;
-            current2 = current2.next;
-        }
-
-        return dummy.next;
+        return res;
     }
- 
 
-    public class ListNode
+    private static void DFS(TreeNode node, List<int>? res)
+    {
+        if (node == null) return;
+        DFS(node.left, res);
+        res.Add(node.val);
+        DFS(node.right, res);
+    }
+
+
+    public class TreeNode
     {
         public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
         {
             this.val = val;
-            this.next = next;
+            this.left = left;
+            this.right = right;
         }
     }
 
     static void Main(string[] args)
     {
-        //list1 = [1,2,4], list2 = [1,3,4]
-        ListNode list1 = new ListNode(1);
-        list1.next = new ListNode(2);
-        list1.next.next = new ListNode(4);
+        //list1 = [1,null,2,3] 
+        TreeNode root = new TreeNode(1);
+        root.left = null;
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
 
-        var list2 = new ListNode(1);
-        list2.next = new ListNode(3);
-        list2.next.next = new ListNode(4);
-
-        var res = MergeTwoLists(list1, list2);
+        var res = InorderTraversal(root);
 
         Console.WriteLine(res);
     }
