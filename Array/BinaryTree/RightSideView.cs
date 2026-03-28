cde686329073eef4438848b2a71f7c237f161cb6
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-03-28 21:45:45 +0200

    RightSideViewProblem

---
+++ Array/BinaryTree/RightSideView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.BinaryTree
+{
+    internal class RightSideViewProblem
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            var result = new List<int>();
+
+            var queue = new Queue<TreeNode>();
+
+            if (root != null)
+            {
+                queue.Enqueue(root);
+            }
+
+
+            while (queue.Count > 0)
+            {
+                var levelSize = queue.Count;
+
+                for (int i = 0; i < levelSize; i++)
+                {
+                    var current = queue.Dequeue();
+
+                    if (i == levelSize - 1)
+                    {
+                        result.Add(current.val);
+                    }
+
+                    if (current.left != null)
+                    {
+                        queue.Enqueue(current.left);
+                    }
+
+                    if (current.right != null)
+                    {
+                        queue.Enqueue(current.right);
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
