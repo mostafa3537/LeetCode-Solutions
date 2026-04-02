6eef4f01974504d5cca4b387e9bb6830decbaf33
Mostafa Ahmed Elsayed <melsayed@RASAN.CO>
2026-04-02 16:12:50 +0200

    WordPattern

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,36 +1,54 @@
 ﻿namespace Problems;
 
-using System;
-using System.Collections.Generic;
-using System.Reflection.Metadata.Ecma335;
-using System.Xml.Linq;
-
 internal class Program
 {
-    public static bool FindTheDifference(string pattern, string s)
+    public static bool WordPattern(string pattern, string s)
     {
-        var result = false;
-
-        var dic = new Dictionary<char, string>();
-
-
-
-
-        for (int i = 0; i < pattern.Length; i++)
-        {
-            dic[i] = pattern[i];
-        }
+        var wordToChar = new Dictionary<string, char>();
+        var charToWord = new Dictionary<char, string>();
         
-        return result;
+        var words = s.Split(' ');
+		if (words.Length != pattern.Length)
+		{
+			return false;
+		}
+		for (int i = 0; i < pattern.Length; i++)
+        {
+            var word = words[i];
+
+            var patt = pattern[i];
+
+            if (wordToChar.TryGetValue(words[i], out char val))
+            {
+                if (val != pattern[i])
+                {
+                    return false;
+                }
+            }
+
+			if (charToWord.TryGetValue(pattern[i], out string cha))
+			{
+				if (cha != words[i])
+				{
+					return false;
+				}
+			}
+
+			wordToChar[words[i]] = pattern[i];
+			charToWord[pattern[i]] = words[i];
+        }
+
+
+		return true;
     }
 
 
     static void Main(string[] args)
     {
         var pattern = "abba";
-        var t = "dog cat cat dog";
+        var t = "dog cat cat fish";
 
-        var res = FindTheDifference(pattern, t);
+        var res = WordPattern(pattern, t);
         Console.WriteLine(res);
     }
 }
