77c2d67663f6686e947324ab30c1ee179d5e161a
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-04-03 06:19:06 +0200

    AllOne

---
+++ Array/HashTablesProblems/AllOneProblem.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HashTablesProblems
+{
+    internal class AllOneProblem
+    {
+        public class AllOne
+        {
+
+            public class Node
+            {
+                public HashSet<string> Keys { get; set; }
+                public int Count { get; set; }
+                public Node Prev { get; set; }
+                public Node Next { get; set; }
+                public Node(int count)
+                {
+                    Count = count;
+                    Keys = new HashSet<string>();
+                }
+            }
+
+            private Dictionary<string, Node> keyCount;
+
+            private Node head;
+            private Node tail;
+
+            private string maxKey;
+            private string minkey;
+            public AllOne()
+            {
+                keyCount = new Dictionary<string, Node>();
+                minkey = string.Empty;
+                maxKey = string.Empty;
+
+                head = new Node(0);
+                tail = new Node(0);
+
+                head.Next = tail;
+                tail.Prev = head;
+            }
+
+            public void Inc(string key)
+            {
+                // if key is not present, add it to the head node
+                // if key is present, move it to the next node
+
+                if (!keyCount.ContainsKey(key))
+                {
+                    var node = head.Next;
+
+                    if (node == tail || node.Count != 1)
+                    {
+                        // insert new node with count 1 after head
+
+                        var newNode = new Node(1);
+                        node = InsertAfter(head, newNode);
+                    }
+                    node.Keys.Add(key);
+                    keyCount[key] = node;
+                }
+                else
+                {
+                    // find the node with the key remove the key from the node and add it to the next node
+
+                    var currentNode = keyCount[key];
+                    var nextNode = currentNode.Next;
+
+                    if (currentNode == tail || nextNode.Count != currentNode.Count + 1)
+                    {
+                        // insert new node with count 1 after head
+                        nextNode = InsertAfter(currentNode, new Node(currentNode.Count + 1));
+                    }
+
+                    nextNode.Keys.Add(key);
+                    keyCount[key] = nextNode;
+
+                    RemoveKey(currentNode, key);
+                }
+
+            }
+
+            private void RemoveKey(Node currentNode, string key)
+            {
+                currentNode.Keys.Remove(key);
+                if (currentNode.Keys.Count == 0)
+                {
+                    // remove the node from the list
+                    currentNode.Prev.Next = currentNode.Next;
+                    currentNode.Next.Prev = currentNode.Prev;
+                }
+            }
+
+            private Node InsertAfter(Node head, Node newNode)
+            {
+                // insert newNode after head
+
+                newNode.Prev = head;
+
+                newNode.Next = head.Next;
+
+                head.Next.Prev = newNode;
+
+                head.Next = newNode;
+                return newNode;
+            }
+
+            public void Dec(string key)
+            {
+                if (keyCount.ContainsKey(key))
+                {
+                    // find the node with the key remove the key from the node  
+                    var currentNode = keyCount[key];
+                    var prevNode = currentNode.Prev;
+
+                    if (currentNode.Count == 1)
+                    {
+                        RemoveKey(currentNode, key);
+                        keyCount.Remove(key);
+                        return;
+                    }
+
+                    if (currentNode == head || prevNode.Count != currentNode.Count - 1)
+                    {
+                        // insert new node with count 1 after head
+                        prevNode = InsertAfter(currentNode.Prev, new Node(currentNode.Count - 1));
+                    }
+                    prevNode.Keys.Add(key);
+                    keyCount[key] = prevNode;
+                    RemoveKey(currentNode, key);
+                }
+
+            }
+
+            public string GetMaxKey()
+            {
+                return tail.Prev.Keys.FirstOrDefault() ?? "";
+            }
+
+            public string GetMinKey()
+            {
+                return head.Next.Keys.FirstOrDefault() ?? "";
+            }
+        }
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -3,111 +3,161 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
 using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 internal class Program
 {
-    public static int MinAreaRect(int[][] points)
+    public class AllOne
     {
-        var area = int.MaxValue;
 
-        var xDict = new Dictionary<int, List<int[]>>();
-        var yDict = new Dictionary<int, List<int[]>>();
-
-        // Group points by x and y coordinates
-
-        for (int i = 0; i < points.Length; i++)
+        public class Node
         {
-            if (xDict.TryGetValue(points[i][0], out List<int[]> val))
+            public HashSet<string> Keys { get; set; }
+            public int Count { get; set; }
+            public Node Prev { get; set; }
+            public Node Next { get; set; }
+            public Node(int count)
             {
-                val.Add(points[i]);
+                Count = count;
+                Keys = new HashSet<string>();
+            }
+        }
+
+        private Dictionary<string, Node> keyCount;
+
+        private Node head;
+        private Node tail;
+
+        private string maxKey;
+        private string minkey;
+        public AllOne()
+        {
+            keyCount = new Dictionary<string, Node>();
+            minkey = string.Empty;
+            maxKey = string.Empty;
+
+            head = new Node(0);
+            tail = new Node(0);
+
+            head.Next = tail;
+            tail.Prev = head;
+        }
+
+        public void Inc(string key)
+        {
+            // if key is not present, add it to the head node
+            // if key is present, move it to the next node
+
+            if (!keyCount.ContainsKey(key))
+            {
+                var node = head.Next;
+
+                if (node == tail || node.Count != 1)
+                {
+                    // insert new node with count 1 after head
+
+                    var newNode = new Node(1);
+                    node = InsertAfter(head, newNode);
+                }
+                node.Keys.Add(key);
+                keyCount[key] = node;
             }
             else
             {
-                xDict[points[i][0]] = new List<int[]> { points[i] };
+                // find the node with the key remove the key from the node and add it to the next node
+
+                var currentNode = keyCount[key];
+                var nextNode = currentNode.Next;
+
+                if (currentNode == tail || nextNode.Count != currentNode.Count + 1)
+                {
+                    // insert new node with count 1 after head
+                    nextNode = InsertAfter(currentNode, new Node(currentNode.Count + 1));
+                }
+
+                nextNode.Keys.Add(key);
+                keyCount[key] = nextNode;
+
+                RemoveKey(currentNode, key);
             }
 
-            if (yDict.TryGetValue(points[i][1], out List<int[]> val2))
-            {
-                val2.Add(points[i]);
-            }
-            else
-            {
-                yDict[points[i][1]] = new List<int[]> { points[i] };
-            }
         }
 
-
-        for (int i = 0; i < points.Length; i++)
+        private void RemoveKey(Node currentNode, string key)
         {
-            for (int j = i + 1; j < points.Length; j++)
+            currentNode.Keys.Remove(key);
+            if (currentNode.Keys.Count == 0)
             {
-                if (i == j)
-                {
-                    continue;
-                }
-
-                if (points[i][0] == points[j][0] || points[i][1] == points[j][1])
-                {
-                    continue;
-                }
-
-                var currentpoint1 = points[i];
-                var currentpoint2 = points[j];
-                var point3Found = false;
-                var point4found = false;
-                if (xDict.TryGetValue(currentpoint1[0], out List<int[]> xList))
-                {
-                    foreach (var point in xList)
-                    {
-                        if (point[1] == currentpoint2[1])
-                        {
-                            point3Found = true;
-                            break;
-                        }
-                    }
-
-                }
-                if (yDict.TryGetValue(currentpoint1[1], out List<int[]> yList))
-                {
-                    foreach (var point in yList)
-                    {
-                        if (point[0] == currentpoint2[0])
-                        {
-                            point4found = true;
-                            break;
-                        }
-                    }
-
-                }
-
-                if (point3Found && point4found)
-                {
-                    area = Math.Min(area, Math.Abs(currentpoint1[0] - currentpoint2[0]) * Math.Abs(currentpoint1[1] - currentpoint2[1]));
-                }
+                // remove the node from the list
+                currentNode.Prev.Next = currentNode.Next;
+                currentNode.Next.Prev = currentNode.Prev;
             }
         }
 
-        if (area == int.MaxValue)
+        private Node InsertAfter(Node head, Node newNode)
         {
-            return 0;
+            // insert newNode after head
+
+            newNode.Prev = head;
+
+            newNode.Next = head.Next;
+
+            head.Next.Prev = newNode;
+
+            head.Next = newNode;
+            return newNode;
         }
-        return area;
+
+        public void Dec(string key)
+        {
+            if (keyCount.ContainsKey(key))
+            {
+                // find the node with the key remove the key from the node  
+                var currentNode = keyCount[key];
+                var prevNode = currentNode.Prev;
+
+                if (currentNode.Count == 1)
+                {
+                    RemoveKey(currentNode, key);
+                    keyCount.Remove(key);
+                    return;
+                }
+
+                if (currentNode == head || prevNode.Count != currentNode.Count - 1)
+                {
+                    // insert new node with count 1 after head
+                    prevNode = InsertAfter(currentNode.Prev, new Node(currentNode.Count - 1));
+                }
+                prevNode.Keys.Add(key);
+                keyCount[key] = prevNode;
+                RemoveKey(currentNode, key);
+            }
+    
+        }
+
+        public string GetMaxKey()
+        {
+            return tail.Prev.Keys.FirstOrDefault() ?? "";
+        }
+
+        public string GetMinKey()
+        {
+            return head.Next.Keys.FirstOrDefault() ?? "";
+        }
     }
-
-
     static void Main(string[] args)
     {
-        var list = new int[][]
-        {
-            new[] { 1, 1 },
-            new[] { 1, 3 },
-            new[] { 3, 1 },
-            new[] { 3, 3 },
-            new[] { 2, 2 }
-        };
+        AllOne allOne = new AllOne();
+        allOne.Inc("hello");
+        allOne.Inc("hello");
+        allOne.GetMaxKey(); // return "hello"
+        allOne.GetMinKey(); // return "hello"
+        allOne.Inc("leet");
+        allOne.GetMaxKey(); // return "hello"
+        allOne.GetMinKey(); // return "leet"
 
-        var res = MinAreaRect(list);
-        Console.WriteLine(res);
+        Console.WriteLine();
     }
 }
