0fca869cda056b5621befda1390996228fa59554
mostafa3537 <mostafa.elsayed3537@gmail.com>
2026-04-03 08:34:04 +0200

    SortedArrayToBST

---
+++ Array/BinaryTreeProblems/SortedArrayToBSTProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Problems.BinaryTree;
+
+namespace Problems.BinaryTreeProblems
+{
+    internal class SortedArrayToBSTProblem
+    {
+        public static TreeNode SortedArrayToBST(int[] nums)
+        {
+            var root = BuildTree(nums, 0, nums.Length - 1);
+
+            return root;
+        }
+
+        private static TreeNode BuildTree(int[] nums, int left, int right)
+        {
+            if (left > right) return null;
+            var mid = left + (right - left) / 2;
+            var node = new TreeNode(nums[mid]);
+            node.left = BuildTree(nums, left, mid - 1);
+            node.right = BuildTree(nums, mid + 1, right);
+            return node;
+        }
+
+    }
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -5,159 +5,45 @@ using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.ConstrainedExecution;
 using System.Xml.Linq;
+using Problems.BinaryTree;
 using static System.Net.Mime.MediaTypeNames;
 
 internal class Program
 {
-    public class AllOne
+    public static TreeNode SortedArrayToBST(int[] nums)
     {
+        var root = BuildTree(nums, 0, nums.Length - 1);
 
-        public class Node
+        return root;
+    }
+
+    private static TreeNode BuildTree(int[] nums, int left, int right)
+    {
+        if (left > right) return null;
+        var mid = left + (right - left) / 2;
+        var node = new TreeNode(nums[mid]);
+        node.left = BuildTree(nums, left, mid - 1);
+        node.right = BuildTree(nums, mid + 1, right);
+        return node;
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
         {
-            public HashSet<string> Keys { get; set; }
-            public int Count { get; set; }
-            public Node Prev { get; set; }
-            public Node Next { get; set; }
-            public Node(int count)
-            {
-                Count = count;
-                Keys = new HashSet<string>();
-            }
-        }
-
-        private Dictionary<string, Node> keyCount;
-
-        private Node head;
-        private Node tail;
-
-        private string maxKey;
-        private string minkey;
-        public AllOne()
-        {
-            keyCount = new Dictionary<string, Node>();
-            minkey = string.Empty;
-            maxKey = string.Empty;
-
-            head = new Node(0);
-            tail = new Node(0);
-
-            head.Next = tail;
-            tail.Prev = head;
-        }
-
-        public void Inc(string key)
-        {
-            // if key is not present, add it to the head node
-            // if key is present, move it to the next node
-
-            if (!keyCount.ContainsKey(key))
-            {
-                var node = head.Next;
-
-                if (node == tail || node.Count != 1)
-                {
-                    // insert new node with count 1 after head
-
-                    var newNode = new Node(1);
-                    node = InsertAfter(head, newNode);
-                }
-                node.Keys.Add(key);
-                keyCount[key] = node;
-            }
-            else
-            {
-                // find the node with the key remove the key from the node and add it to the next node
-
-                var currentNode = keyCount[key];
-                var nextNode = currentNode.Next;
-
-                if (currentNode == tail || nextNode.Count != currentNode.Count + 1)
-                {
-                    // insert new node with count 1 after head
-                    nextNode = InsertAfter(currentNode, new Node(currentNode.Count + 1));
-                }
-
-                nextNode.Keys.Add(key);
-                keyCount[key] = nextNode;
-
-                RemoveKey(currentNode, key);
-            }
-
-        }
-
-        private void RemoveKey(Node currentNode, string key)
-        {
-            currentNode.Keys.Remove(key);
-            if (currentNode.Keys.Count == 0)
-            {
-                // remove the node from the list
-                currentNode.Prev.Next = currentNode.Next;
-                currentNode.Next.Prev = currentNode.Prev;
-            }
-        }
-
-        private Node InsertAfter(Node head, Node newNode)
-        {
-            // insert newNode after head
-
-            newNode.Prev = head;
-
-            newNode.Next = head.Next;
-
-            head.Next.Prev = newNode;
-
-            head.Next = newNode;
-            return newNode;
-        }
-
-        public void Dec(string key)
-        {
-            if (keyCount.ContainsKey(key))
-            {
-                // find the node with the key remove the key from the node  
-                var currentNode = keyCount[key];
-                var prevNode = currentNode.Prev;
-
-                if (currentNode.Count == 1)
-                {
-                    RemoveKey(currentNode, key);
-                    keyCount.Remove(key);
-                    return;
-                }
-
-                if (currentNode == head || prevNode.Count != currentNode.Count - 1)
-                {
-                    // insert new node with count 1 after head
-                    prevNode = InsertAfter(currentNode.Prev, new Node(currentNode.Count - 1));
-                }
-                prevNode.Keys.Add(key);
-                keyCount[key] = prevNode;
-                RemoveKey(currentNode, key);
-            }
-    
-        }
-
-        public string GetMaxKey()
-        {
-            return tail.Prev.Keys.FirstOrDefault() ?? "";
-        }
-
-        public string GetMinKey()
-        {
-            return head.Next.Keys.FirstOrDefault() ?? "";
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
     static void Main(string[] args)
     {
-        AllOne allOne = new AllOne();
-        allOne.Inc("hello");
-        allOne.Inc("hello");
-        allOne.GetMaxKey(); // return "hello"
-        allOne.GetMinKey(); // return "hello"
-        allOne.Inc("leet");
-        allOne.GetMaxKey(); // return "hello"
-        allOne.GetMinKey(); // return "leet"
+        var nums = new int[] { -10, -3, 0, 5, 9 };
 
+        var res = SortedArrayToBST(nums);
         Console.WriteLine();
     }
 }
