7b340278909721dd4cb446e4f1881c4453fcc73e
Mostafa Ahmed Elsayed <melsayed@RASAN.CO>
2026-05-13 14:03:11 +0300

    RandomizedSet

---
+++ Array/HashTablesProblems/RandomizedSetProblem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.HashTablesProblems
+{
+	internal class RandomizedSetProblem
+	{
+		public class RandomizedSet
+		{
+			public Dictionary<int, int> KeyValues { get; set; }
+
+			public RandomizedSet()
+			{
+				KeyValues = new Dictionary<int, int>();
+			}
+
+			public bool Insert(int val)
+			{
+
+				if (KeyValues.ContainsKey(val))
+				{
+					return false;
+				}
+				else
+				{
+					KeyValues.Add(val, val);
+					return true;
+				}
+			}
+
+			public bool Remove(int val)
+			{
+				if (KeyValues.TryGetValue(val, out int val2))
+				{
+					KeyValues.Remove(val);
+
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+
+			}
+
+			public int GetRandom()
+			{
+				var random = new Random();
+
+				int value = random.Next(0, KeyValues.Count);
+
+				return KeyValues.ElementAt(value).Value;
+			}
+		}
+	}
+}
--- Array/Program.cs
+++ Array/Program.cs
@@ -2,107 +2,66 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.ConstrainedExecution;
-using System.Xml.Linq;
-using Problems.BinaryTree;
-using static System.Net.Mime.MediaTypeNames;
+
 
 internal class Program
 {
+	public class RandomizedSet
+	{
+		public Dictionary<int, int> KeyValues { get; set; }
 
-    public TreeNode BuildTree(int[] preorder, int[] inorder)
-    {
-        var tree = new TreeNode();
+		public RandomizedSet()
+		{
+			KeyValues = new Dictionary<int, int>();
+		}
 
-        if (preorder.Length < 1 || inorder.Length < 1)
-        {
-            return tree;
-        }
+		public bool Insert(int val)
+		{
 
-        Build(tree, preorder, inorder);
+			if (KeyValues.ContainsKey(val)) 
+			{
+				return false;
+			}
+			else
+			{
+				KeyValues.Add(val, val);
+				return true;
+			}
+		}
 
-        return tree;
-    }
+		public bool Remove(int val)
+		{
+			if (KeyValues.TryGetValue(val, out int val2))
+			{
+				KeyValues.Remove(val);
 
-    private static void Build(TreeNode tree, int[] preorder, int[] inorder)
-    {
-        var rootValue = preorder.First();
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 
-        tree.val = rootValue;
+		}
 
-        var rootIndexInorder = Array.IndexOf(inorder, rootValue);
+		public int GetRandom()
+		{
+			var random = new Random();
 
-        var leftInorder = inorder.Take(rootIndexInorder).ToArray();
-        var rightInorder = inorder.Skip(rootIndexInorder + 1).ToArray();
+			int value = random.Next(0, KeyValues.Count);
 
-        var leftPreorder = preorder.Skip(1).Take(leftInorder.Length).ToArray();
-        var rightPreorder = preorder.Skip(1 + leftInorder.Length).ToArray();
+			return KeyValues.ElementAt(value).Value;
+		}
+	}
 
-        if (leftPreorder.Length > 0)
-        {
-            tree.left = new TreeNode();
-            Build(tree.left, leftPreorder, leftInorder);
-        }
-
-        if (rightPreorder.Length > 0)
-        {
-            tree.right = new TreeNode();
-            Build(tree.right, rightPreorder, rightInorder);
-        }
-
-    }
-    public static List<int> Pre(TreeNode tree)
-    {
-        var res = new List<int>();
-
-        PreOrder(tree, res);
-
-        return res;
-    }
-
-
-    public static void PreOrder(TreeNode tree, List<int> res)
-    {
-        if (tree == null)
-        {
-            return;
-        }
-
-        res.Add(tree.val);
-
-        PreOrder(tree.left, res);
-        PreOrder(tree.right, res);
-
-    }
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    static void Main(string[] args)
-    {
-        //[3,9,20,null,null,15,7]
-        TreeNode tree = new(3);
-
-        tree.left = new(9);
-        tree.right = new(20);
+	static void Main(string[] args)
+	{
+		RandomizedSet obj = new RandomizedSet();
+		bool param_1 = obj.Insert(1);
+		bool param_2 = obj.Remove(2);
+		int param_3 = obj.GetRandom();
 
 
-        tree.right.left = new(15);
-        tree.right.right = new(7);
-
-        var res = Pre(tree);
-        Console.WriteLine();
-    }
+		Console.WriteLine();
+	}
 }
